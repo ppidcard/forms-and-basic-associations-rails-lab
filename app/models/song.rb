7fae9fc9c36c3695042142496a7e8eaae3da8634
ppidcard <tvxqlikei@live.cn>
2021-10-10 14:15:33 +1100

    Done

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,32 @@
 class Song < ActiveRecord::Base
   # add associations here
+  belongs_to :artist
+  belongs_to :genre
+  has_many :notes
+  # accepts_nested_attributes_for :notes
+
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end 
+
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end 
+
+  def note_contents=(contents)
+    contents.each do |c|
+      note = Note.new
+      note.content = c
+      self.notes << note
+    end 
+  end 
+
+  def note_contents
+    contents = []
+    self.notes.map do |n|
+      contents << n.content
+    end
+    contents
+  end
 end
